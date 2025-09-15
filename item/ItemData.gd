bbbfad5c2a202bb4ae9b4684b559997d45cc90b7
Justin <59674603+Toash@users.noreply.github.com>
2025-09-14 20:02:29 -0400

    add tilemap

---
+++ item/ItemData.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name ItemData
+
+@export var id: String
+
+@export var display_name: String
+@export var description: String
+@export var max_stack: int
+
+
+@export var icon: Texture2D
---
+++ item/ItemInstance.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name ItemInstance
+
+@export var item_data: ItemData
+
+@export var amount: int
+
+@export var max_durability: int
+@export var durability: int
