2a66c4b811315ac4ba8517bbf6e2a068e94d0ce8
Justin <59674603+Toash@users.noreply.github.com>
2025-11-14 00:39:19 -0500

    update

--- actors/pawns/generic/hurt_sound.gd
+++ actors/pawns/generic/hurt_sound.gd
@@ -4,13 +4,18 @@ class_name HurtSound
 
 
 @export var health: Health
-@export var audio_key: AudioManager.KEY
+@export var hit: AudioManager.KEY
+@export var death: AudioManager.KEY
 @export var bus: String = "master"
 
 
 func _ready():
 	health.health_changed.connect(_on_health_changed)
+	health.died.connect(_on_died)
 
 
 func _on_health_changed(new_val: int):
-	AudioManager.play_key(audio_key, 1, global_position, bus)
+	AudioManager.play_key(hit, 1, global_position, bus)
+
+func _on_died():
+	AudioManager.play_key(death, 1, global_position, bus)
